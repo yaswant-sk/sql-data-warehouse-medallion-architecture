146fceb285f061e11f3aa1015c3b5e0567f66784
yaswant-sk <yaswant@susneo.com>
2026-05-20 15:56:19 +0100

    Create silver schema tables in ddl_silver.sql
    This script creates multiple tables in the 'silver' schema, including customer info, product info, sales details, and various ERP tables. Existing tables are dropped before creation to ensure the DDL structure is redefined.

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,81 @@
+/*
+===============================================================================
+DDL Script: Create silver Tables (PostgreSQL)
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+    
+    Run this script to re-define the DDL structure of 'silver' tables.
+===============================================================================
+*/
+
+DROP TABLE IF EXISTS silver.crm_cust_info;
+
+CREATE TABLE silver.crm_cust_info (
+    cst_id              INTEGER,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.crm_prd_info;
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id         INTEGER,
+    prd_key        VARCHAR(50),
+    prd_nm         VARCHAR(50),
+    prd_cost       NUMERIC,
+    prd_line       VARCHAR(50),
+    prd_start_dt   TIMESTAMP,
+    prd_end_dt     TIMESTAMP,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.crm_sales_details;
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num    VARCHAR(50),
+    sls_prd_key    VARCHAR(50),
+    sls_cust_id    INTEGER,
+    sls_order_dt   INTEGER,
+    sls_ship_dt    INTEGER,
+    sls_due_dt     INTEGER,
+    sls_sales      INTEGER,
+    sls_quantity   INTEGER,
+    sls_price      INTEGER,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid            VARCHAR(50),
+    cntry          VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid            VARCHAR(50),
+    bdate          DATE,
+    gen            VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id             VARCHAR(50),
+    cat            VARCHAR(50),
+    subcat         VARCHAR(50),
+    maintenance    VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
